d65f0b2ebf2a12cda4cbf8c9017a1e829e8f31db
TarekMahmoud175 <tarek64@live.com>
2023-10-07 01:23:04 +0300

    created button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import ButtonComponent from "@/components/atoms/button";
 
 export default function Home() {
-  return <></>;
+  return <ButtonComponent children={"hiiiiii"} />;
 }
---
+++ src/components/atoms/button/index.tsx
@@ -0,0 +1,27 @@
+import React, { ButtonHTMLAttributes, ReactNode } from "react";
+import styles from "./button.module.scss";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  className?: string;
+  onClick?: () => void;
+}
+
+const ButtonComponent = ({
+  onClick,
+  className,
+  children,
+  ...rest
+}: ButtonProps) => {
+  return (
+    <button
+      className={`${className} ${styles.button}`}
+      onClick={onClick}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default ButtonComponent;
