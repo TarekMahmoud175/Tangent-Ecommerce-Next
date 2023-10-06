1c30aa8960f9f42b8ac652f0a69f4d3876297027
TarekMahmoud175 <tarek64@live.com>
2023-10-07 00:53:01 +0300

    added git commiting file

---
+++ commit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "there is no commit message"
+  exit 0
+fi
+
+git add .
+
+git commit -m "$1"
+
+# change "main" with your branch name.
+branch=main;
+
+git pull origin $branch
+
+git push origin $branch
+
+exit 0
