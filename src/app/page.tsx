ffc2f9432bf31d398e45c576f05b3a342ee62043
TarekMahmoud175 <tarek64@live.com>
2023-10-07 13:09:41 +0300

    added navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ButtonComponent from "@/components/atoms/button";
 import styles from "./page.module.scss";
 import InputComponent from "@/components/atoms/input";
+import NavbarComponent from "@/components/molcules/navbar";
 
 export default function Home() {
   return (
     <React.Fragment>
+      <NavbarComponent />
       <div className="container">
         <div className="row">
           <div className="col-6">
